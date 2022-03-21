cd7dfdc0f7a07ae7fd4987e6166060488c2b3310
tuan <tuanbosstrum@gmail.com>
2022-03-21 21:15:08 +0700

    --- Auto-Git Commit ---

--- static/js/style.js
+++
@@ -1,9 +0,0 @@
-let container = document.querySelector(".container");
-let random = document.querySelector(".random");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 1000);
-
-btn.onclick = function() {
-    container.style.transform = "rotate(" + number + "deg)";
-    number += Math.ceil(Math.random() * 1000);
-}
