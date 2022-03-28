05420dba5dd38722f842c970cde6165bc01e37e2
tuan <tuanbosstrum@gmail.com>
2022-03-28 19:35:35 +0700

    --- Auto-Git Commit ---

--- static/js/style.js
+++ static/js/style.js
@@ -1,4 +1,4 @@
-let rads = [];
+/**let rads = []; 
 
 function checkrand(min, max) {
     let rand = Math.floor((Math.random() * (max - min)) + min);
@@ -56,4 +56,63 @@ function checkve(min, max) {
             break;
     }
 
+}*/
+
+var numrad;
+
+function add() {
+    if (rads.length < 9) {
+        numrad = setInterval(rand, 100);
+    }
+    document.getElementById("addrand").style.display = "none";
+
+
 }
+
+let rads = [];
+
+function checkrand(min, max) {
+    let rand = Math.floor((Math.random() * (max - min)) + min);
+    if (!rads.includes(Math.floor(Math.random() * (max - min)) + min)) {
+        rads.push(rand);
+
+    } else {
+        checkrand(min, max);
+    }
+    return rand;
+}
+
+function rand() {
+    document.getElementById("giaidacbiet").innerHTML = checkrand(300, 100);
+    document.getElementById("giainhat").innerHTML = checkrand(300, 100);
+    document.getElementById("giainhi").innerHTML = checkrand(300, 100);
+
+    document.getElementById("giaiba").innerHTML = checkrand(300, 100);
+
+    document.getElementById("giaitu").innerHTML = checkrand(300, 100);
+
+    document.getElementById("giainam").innerHTML = checkrand(300, 100);
+
+    document.getElementById("giaisau").innerHTML = checkrand(300, 100);
+
+    document.getElementById("giaibay").innerHTML = checkrand(300, 100);
+
+
+}
+
+function stopRad() {
+    clearInterval(numrad);
+
+}
+
+function startTime() {
+    const date = new Date();
+    document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+    setTimeout(function() { startTime() }, 1000);
+}
+document.querySelector('jsuites-calendar').addEventListener('onchange', function(e) {
+    console.log('New value: ' + e.target.value);
+});
+document.querySelector('jsuites-calendar').addEventListener('onclose', function(e) {
+    console.log('Calendar is closed');
+});
