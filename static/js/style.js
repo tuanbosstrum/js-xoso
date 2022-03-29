c9b28a7cd16f5a7435d83c1e4ee378471b8b4f82
tuan <tuanbosstrum@gmail.com>
2022-03-29 07:35:12 +0700

    ss

--- static/js/style.js
+++ static/js/style.js
@@ -1,48 +1,32 @@
 var numrad;
 
 function add() {
-    if (rads.length < 9) {
-        numrad = setInterval(rand, 100);
-    }
+    numrad = setInterval(rand, 100);
     document.getElementById("addrand").style.display = "none";
 }
 
-let rads = [];
-
-function checkrand(min, max) {
-    let rand = Math.floor((Math.random() * (max - min)) + min);
-    if (!rads.includes(Math.floor(Math.random() * (max - min)) + min)) {
-        rads.push(rand);
-
-    } else {
-        checkrand(min, max);
-    }
-    return rand;
-}
 
 function rand() {
-    document.getElementById("giaidacbiet").innerHTML = checkrand(200, 100);
-    document.getElementById("giainhat").innerHTML = checkrand(200, 100);
-    document.getElementById("giainhi").innerHTML = checkrand(200, 100);
-    document.getElementById("giaiba").innerHTML = checkrand(200, 100);
-    document.getElementById("giaitu").innerHTML = checkrand(200, 100);
-    document.getElementById("giainam").innerHTML = checkrand(200, 100);
-    document.getElementById("giaisau").innerHTML = checkrand(200, 100);
-    document.getElementById("giaibay").innerHTML = checkrand(200, 100);
+    document.getElementById("giaidacbiet").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giainhat").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giainhi").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giaiba").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giaitu").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giainam").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giaisau").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+    document.getElementById("giaibay").innerHTML =
+        Math.floor(Math.random() * (300 - 100)) + 100;
+
 }
 
 function stopRad() {
     clearInterval(numrad);
-}
 
-function startTime() {
-    const date = new Date();
-    document.getElementById("demo").innerHTML = date.toLocaleTimeString();
-    setTimeout(function() { startTime() }, 1000);
 }
-document.querySelector('jsuites-calendar').addEventListener('onchange', function(e) {
-    console.log('New value: ' + e.target.value);
-});
-document.querySelector('jsuites-calendar').addEventListener('onclose', function(e) {
-    console.log('Calendar is closed');
-});
