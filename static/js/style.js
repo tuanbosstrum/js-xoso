f6b06caeabe3958214a89215a5183654c03ba6ee
tuan <tuanbosstrum@gmail.com>
2022-03-30 09:14:17 +0700

    --- Auto-Git Commit ---

--- static/js/style.js
+++ static/js/style.js
@@ -8,25 +8,24 @@ function add() {
 
 function rand() {
     document.getElementById("giaidacbiet").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giainhat").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giainhi").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giaiba").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giaitu").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giainam").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giaisau").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giaibay").innerHTML =
-        Math.floor(Math.random() * (300 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100;
 
 }
 
 function stopRad() {
     clearInterval(numrad);
-
 }
