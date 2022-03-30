ab1bbef9a35de3fba440acd0e152f5365cf5fa1f
tuan <tuanbosstrum@gmail.com>
2022-03-30 11:55:30 +0700

    --- Auto-Git Commit ---

--- static/js/style.js
+++ static/js/style.js
@@ -2,7 +2,7 @@ var numrad;
 
 function add() {
     numrad = setInterval(rand, 100);
-    document.getElementById("addrand").style.display = "none";
+
 }
 
 
@@ -29,3 +29,14 @@ function rand() {
 function stopRad() {
     clearInterval(numrad);
 }
+
+var timeleft = 10;
+var numrad = setInterval(function() {
+    if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Finished";
+    } else {
+        document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+}, 1000);
