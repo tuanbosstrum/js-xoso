9ea881f0279f13ebb62cd6d1d4beef30f89699ad
tuan <tuanbosstrum@gmail.com>
2022-03-31 20:34:57 +0700

    click từng giải

--- static/js/style.js
+++ static/js/style.js
@@ -1,13 +1,15 @@
 var numrad;
+var key = 0;
 
 function add() {
+    key++;
     numrad = setInterval(rand, 100);
 
 }
 
 
 function rand() {
-    document.getElementById("giaidacbiet").innerHTML =
+    /** document.getElementById("giaidacbiet").innerHTML =
         Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giainhat").innerHTML =
         Math.floor(Math.random() * (200 - 100)) + 100;
@@ -22,7 +24,50 @@ function rand() {
     document.getElementById("giaisau").innerHTML =
         Math.floor(Math.random() * (200 - 100)) + 100;
     document.getElementById("giaibay").innerHTML =
-        Math.floor(Math.random() * (200 - 100)) + 100;
+        Math.floor(Math.random() * (200 - 100)) + 100; */
+
+
+    switch (key) {
+        case 1:
+            var db = Math.floor(Math.random() * (300 - 100)) + 100;
+            document.getElementById("giaidacbiet").innerHTML = db;
+
+            break;
+        case 2:
+            var gn = Math.floor(Math.random() * (300 - 100)) + 100;
+            document.getElementById("giainhat").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+
+            break;
+        case 3:
+            document.getElementById("giainhi").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            break;
+        case 4:
+            document.getElementById("giaiba").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            break;
+        case 5:
+            document.getElementById("giaitu").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            break;
+        case 6:
+            document.getElementById("giainam").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            break;
+        case 7:
+            document.getElementById("giaisau").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            break;
+        case 8:
+            document.getElementById("giaibay").innerHTML =
+                Math.floor(Math.random() * (300 - 100)) + 100;
+            document.getElementById("message").innerHTML = "Chúc mừng bạn đã trúng giải đặc biệt";
+            break;
+
+        default:
+            break;
+    }
 
 }
 
